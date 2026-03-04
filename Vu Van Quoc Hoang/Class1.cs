927dd12bdcdee027c55d75330d60b5e3f5d40c47
BanhBaoz <q.vuvan05@gmail.com>
2026-03-04 18:02:13 +0100

    vu van quoc hoang

---
+++ Vu Van Quoc Hoang/Class1.cs
@@ -0,0 +1,55 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lab2;
+
+public class StringCalculator
+{
+    public static int Calculate(string arg)
+    {
+        if(arg.Length== 0)  return 0;
+
+    
+
+        List<string> delims = new List<string>() { ",", "\n" };
+        if (arg.StartsWith("//"))
+        {
+            string[] parts = arg.Split('\n', 2);
+            string header = parts[0].Substring(2);
+
+            if (header.StartsWith("["))
+            {
+                string[] dels = header.Split('[', ']', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string str in dels)
+                {
+                    if (string.IsNullOrEmpty(str)) continue;
+                    delims.Add(str);
+                }
+            }
+            else
+            {
+                delims.Add(header);
+            }
+
+            arg = parts[1];
+        }
+        string[] strings = arg.Split(delims.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+        int res = 0;
+        foreach(var str in strings)
+        {
+            if(int.TryParse(str,out int r))
+            {
+                if (r < 0)
+                    throw new ArgumentException("Numbers cannot be negative");
+                if (r > 1000) continue;
+                res += r;
+            }
+        }
+
+        //List<string> delimiters = arg.Split("[,]");
+        return res;
+    }
+
+
+}
---
+++ Vu Van Quoc Hoang/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using Lab2;
+using System.Security;
+
+namespace StringCalculatorTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void EmptyStringShouldReturnZeri()
+    {
+        int res = StringCalculator.Calculate(string.Empty);
+
+        Assert.Equal(0, res);
+    }
+
+    [Fact]
+    public void SingleNumberReturnsValue()
+    {
+        string arg = "9";
+
+        if (int.TryParse(arg, out int r))
+        {
+            int res = StringCalculator.Calculate("9");
+            Assert.Equal(9, res);
+        }
+    }
+    [Fact]
+    public void CommaDelimitedReturnsSum()
+    {
+        string arg = "1,2";
+        string[] substrings = arg.Split(',');
+        int sum = 0;
+
+        foreach (string substring in substrings)
+        {
+            if (int.TryParse(substring, out int r))
+                sum += r;
+        }
+
+        int res = StringCalculator.Calculate(arg);
+
+        Assert.Equal(sum, res);
+    } 
+    [Fact]
+    public void NegativeNumbersThrowException()
+    {
+        string arg = "-1";
+        var exception = Assert.Throws<ArgumentException>(() => StringCalculator.Calculate(arg));
+        Assert.Equal("Numbers cannot be negative", exception.Message);
+    }
+    [Fact]
+    public void NumbersGreaterThan1000Ingored()
+    {
+        string arg = "1,1000000,2,60000";
+
+        Assert.Equal(3,StringCalculator.Calculate(arg));
+    }
+
+    [Fact]
+    public void NewLineDelimitedReturnSum()
+    {
+        string arg = "1\n2";
+
+        Assert.Equal(3, StringCalculator.Calculate(arg));
+    }
+
+    [Fact]
+    public void NumbersDelimitedEitherWayReturnSum()
+    {
+        string arg = "1\n3,5";
+
+        Assert.Equal(9,StringCalculator.Calculate(arg));
+    }
+
+   
+    [Fact]
+    public void SingleCharDelimiterDefinedOnTHeFirstLine_ShouldBeValid()
+    {
+        string arg = "//#\n10#2";
+
+        Assert.Equal(12, StringCalculator.Calculate(arg));
+    }
+    [Fact]
+    public void MultipleCharDelimiterDefinedOnTHeFirstLine_ShouldBeValid()
+    {
+        string arg = "//[####]\n1####1000000####2####60000";
+
+        Assert.Equal(3, StringCalculator.Calculate(arg));
+    }
+}
