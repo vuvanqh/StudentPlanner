6f65df1ef9eeff70e01512736f9bceb99d1ae46b
BanhBaoz <q.vuvan05@gmail.com>
2026-03-15 13:56:17 +0100

    feat: add DTO for Personal Events

---
+++ Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/DTOs/UpdatePersonalEventRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentPlanner.Core.Application.Events.PersonalEvents.DTOs
+{
+    internal class UpdatePersonalEventRequest
+    {
+    }
+}
