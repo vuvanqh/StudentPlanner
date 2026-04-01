b7dbcef29b1e769476252eb98a3b67f6faf5be6b
BanhBaoz <q.vuvan05@gmail.com>
2026-04-01 16:14:58 +0200

    feat: add personal event api to frontend

--- Student Planner/StudentPlanner.Frontend/src/components/calendar/Calendar.tsx
+++ Student Planner/StudentPlanner.Frontend/src/components/calendar/Calendar.tsx
@@ -4,7 +4,11 @@ import interactionPlugin from "@fullcalendar/interaction"  // needed for dayClic
 import type { EventContentArg } from '@fullcalendar/core'
 import type { DateClickArg } from '@fullcalendar/interaction'
 import type { EventClickArg } from '@fullcalendar/core'
-
+import type { personalEventResponse } from '../../types/personalEventTypes'
+// import { useState } from "react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useRef } from "react";
+import type { CalendarApi } from '@fullcalendar/core';
 
 const handleEventClick = (arg: EventClickArg) => {
   alert(`Event: ${arg.event.title}`)
@@ -14,17 +18,41 @@ const handleDateClick = (arg: DateClickArg) => {
     alert(arg.dateStr)
 }
 
-export default function Calendar() {
+export default function Calendar({events}:{events: personalEventResponse[]}) {
+  const mappedEvents = events.map(e => ({
+    id: e.id,
+    title: e.title,
+    start: new Date(e.startTime),
+    end: new Date(e.endTime),
+    extendedProps: {
+      description: e.description,
+      location: e.location
+    }
+  }));
+
+  const calendarRef = useRef<any>(null);
+  const changeView = (viewName: "dayGridMonth" | "timeGridWeek") => {
+    const api = calendarRef.current?.getApi();
+    api?.changeView(viewName);
+  };
+
   return <div className="calendar-wrapper">
+    <div style={{ marginBottom: "10px" }}>
+      <button onClick={() => changeView("dayGridMonth")}>Month</button>
+      <button onClick={() => changeView("timeGridWeek")}>Week</button>
+    </div>
+
     <FullCalendar
-    plugins={[ dayGridPlugin, interactionPlugin ]}
+     ref={calendarRef}
+    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
     initialView="dayGridMonth"
     weekends={true}
     firstDay={1}
     eventContent={renderEventContent}   
     events={[
-        { title: 'event 1', date: '2026-03-28' },
-        { title: 'event 2', date: '2019-04-02' }
+        { title: 'event 1', start: '2026-03-28' },
+        { title: 'event 2', start: '2019-04-02' },
+        ...mappedEvents
     ]}
     eventClick={handleEventClick}
     dateClick={handleDateClick}
@@ -36,9 +64,14 @@ export default function Calendar() {
 
 function renderEventContent(eventInfo: EventContentArg) {
   return(
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  )
+    <div style={{ fontSize: '0.75rem' }}>
+      <div>
+        <b>{eventInfo.timeText}</b>
+      </div>
+
+      <div>
+        <i>{eventInfo.event.title}</i>
+      </div>      
+    </div>
+  );
 }
---
+++ Student Planner/StudentPlanner.Frontend/src/types/personalEventTypes.ts
@@ -0,0 +1,25 @@
+export type personalEventResponse = {
+    id: string,
+    title: string,
+    description?: string,
+    startTime: string,
+    endTime: string,
+    location?: string
+}
+
+export type createPersonalEventRequest = {
+    title: string,
+    description?: string,
+    startTime: string,
+    endTime: string,
+    location?: string
+}
+
+export type updatePersonalEventRequest = {
+    id: string,
+    title: string,
+    description?: string,
+    startTime: string,
+    endTime: string,
+    location?: string
+}
